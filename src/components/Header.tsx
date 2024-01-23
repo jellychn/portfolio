73a6b0ad6affe3dcfe2fb01fd0672e0166cde130
Jerry Chen <jchen123.862@gmail.com>
2024-01-24 12:41:08 +1300

    add animation to nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,6 @@
 import "./Header.scss";
 import { useLocation, useNavigate } from "react-router-dom";
 
-
 export default function About(): JSX.Element {
   const navigate = useNavigate();
   const location = useLocation();
@@ -25,9 +24,16 @@ export default function About(): JSX.Element {
     <div className="header fadeIn">
       <div className="divider" />
       {navigation.map((nav) => (
-        <p className={location.pathname === nav.url ? "on" : "off"} onClick={() => navigate(nav.url)}>{nav.name}</p>
+        <div>
+          <p
+            className={location.pathname === nav.url ? "on" : "off"}
+            onClick={() => navigate(nav.url)}
+          >
+            {nav.name}
+            {location.pathname === nav.url && <span>.</span>}
+          </p>
+        </div>
       ))}
     </div>
   );
 }
-
